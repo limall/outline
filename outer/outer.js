32a14e7c2ab7e9066a30aa03b167cb339eef6bf4
limall <2497902705@qq.com>
2017-11-13 20:17:13 +0800

    project organized

--- outer/outer.js
+++
@@ -1,7 +0,0 @@
-let wss=new (require('ws').Server)({port:8888});
-wss.on('connection',(ws)=>{
-    console.log('one connection!');
-    ws.on('message',(msg)=>{
-        console.log(msg);
-    });
-});
