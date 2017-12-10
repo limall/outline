c4f0090121edbb82bc0c80715a9237848cc90977
limall <2497902705@qq.com>
2017-12-10 22:09:55 +0800

    a pure version

---
+++ outline/template/cpp/templete_struct.hpp
@@ -0,0 +1,13 @@
+struct /*Poutline_nodeName*/ : public OStruct {/*children*//*root*/
+};//end
+private: /*nodeName*/() {}
+public:
+	static /*nodeName*/ *pIt() {
+		static /*nodeName*/ *_pInstance;
+		if(_pInstance==NULL) {
+		/*root_declare*/
+/*initInstance*/
+/*initRelation*/
+        }
+    	return _pInstance;
+	}
