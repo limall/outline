5babc67f5ee2356f5eb4d3a61aea71e510809db4
limall <2497902705@qq.com>
2017-12-21 21:19:08 +0800

    add the function that can select which node to export animation

--- outline/components/AutoRecorder.js
+++ outline/components/AutoRecorder.js
@@ -17,49 +17,27 @@ cc.Class({
 
     properties: {
         dst_path:'',
+        exportNodes:[cc.Node],
     },
 
-    // LIFE-CYCLE CALLBACKS:
-
     onLoad () {
-        this.back=new cc.Node('animation_selector');
-        var sprite=this.back.addComponent(cc.Sprite);
-        sprite.spriteFrame=this.backFrame;
-        this.back.scaleX=this.node.width/this.back.width;
-        this.back.scaleY=this.node.height/this.back.height;
-        this.back.parent=this.node;
-
-        var children=this.node.children;
-        var maxIndex=0;
-        for(var i=0;i<children.length;i++){
-            var child=children[i];
-            if(child.zIndex>maxIndex)
-                maxIndex=child.zIndex;
-        }
-        this.back.zIndex=maxIndex+1;
-
         this.anims=[];
         var that=this;
-        function walkOneNode(parent){
-            var animation=parent.getComponent('AnimationRecorder');
+        for (var i=0;i<exportNodes.length;i++){
+            var node=exportNodes[i];
+            var animation=node.getComponent('AnimationRecorder');
             if(animation){
                 var clips=animation.getClips();
                 for(var i=0;i<clips.length;i++){
                     var clipName=clips[i].name;
-                    that.anims.push(new Anim(parent,clipName));
+                    that.anims.push(new Anim(node,clipName));
                 }
             }
-            var children=parent.children;
-            for(var i=0;i<children.length;i++){
-                walkOneNode(children[i]);
-            }
         }
-        walkOneNode(this.node);
 
         this.i=-1;
         this.getData();
     },
-    back:cc.Node,
     getData:function(increments){
         if(this.i>=0){
             this.anims[this.i].increments=increments;
