b26111f486343bb527f960008fe151d4985d54cb
zqf <2497902705@qq.com>
2018-01-22 16:31:18 +0800

    supporting lua

--- outline/builder/lua/buildOutline.js
+++ outline/builder/lua/buildOutline.js
@@ -28,6 +28,64 @@ function isRightProp(propName){
     return false;
 }
 
+function buildMapable(mapAble){
+    var numChars=['0','1','2','3','4','5','6','7','8','9','.','-'];
+    function ifCharIsNumber(char){
+        for(var i=0;i<numChars.length;i++){
+            var numChar=numChars[i];
+            if(numChar===char)
+                return i;
+        }
+        return -1;
+    }
+
+    function isNumber(str){
+        var chars=str.split('');
+        var haveDot=false;
+        for(var i=0;i<chars.length;i++){
+            var char=chars[i];
+            var index=ifCharIsNumber(char);
+            if(index===-1)
+                return false;
+            else{
+                if(index===11&&i!==0)
+                    return false;
+                if(haveDot&&index===10)
+                    return false;
+                if(index===10)
+                    haveDot=true;
+            }
+        }
+        return true;
+    }
+
+    function produceValue(value){
+        if(isNumber(value))
+            return value;
+        else if(value.indexOf('rgba')===0){
+            value=value.replace(/rgba/,'cc.c4b');
+            return value;
+        }else if(value.indexOf('Vec2')===0||value==='true'||value==='false'){
+            return value;
+        }
+        return '"'+value+'"';
+    }
+
+    var luaCode='    extraData={\n';
+    var keyValues=mapAble.split('%o__%');
+    keyValues.forEach(function(keyValue){
+        var seperate=keyValue.indexOf(':');
+        var key=keyValue.substring(0,seperate);
+        var value=keyValue.substring(seperate+1);
+        if(key&&key!=''&&value&&value!=''&&value!='null'){
+            value=produceValue(value);
+            luaCode+='        '+key+'='+value+',\n';
+        }
+    });
+    luaCode+='    },\n';
+    return luaCode;
+}
+
 /**
  * @method buildOneOutline 生成一个outline的lua代码
  * @param {"Object"} outline 一个outline
@@ -39,9 +97,13 @@ function buildOneOutline(outline){
     for(var propName in outline){
         if(isRightProp(propName)&&luaDefault[propName]!==outline[propName]){
             var value=outline[propName];
-            if(propName==='name')
+            if(propName==='mapAble'){
+                luaCode+=buildMapable(value.substring(1,value.length-1));
+            }else{
+                if(propName==='name')
                 value='"'+value+'"';
-            luaCode+='    '+propName+'='+value+',\n';
+                luaCode+='    '+propName+'='+value+',\n';
+            }
             hasDot=true;
         }
     }
