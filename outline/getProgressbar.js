0add3e0613f3e0f5918aeafca25dda39a5dfdab6
limall <2497902705@qq.com>
2018-02-06 21:55:13 +0800

    supporting progressbar

--- outline/getProgressbar.js
+++ outline/getProgressbar.js
@@ -1,13 +1,14 @@
-var spriteSource=require('./getSpriteFrame');
+var spriteSource=require('./spriteSource');
 function getProgressBar(progressbar){
     var str='';
     if(progressbar){
-        str+='isProgressBar:true%o__%'
-        str+='progressbar_spriteFrame:'+spriteSource.getSpriteFrame(progressbar.barSprite)+'%o__%';
+        str+='isProgressBar:true%o__%';
+        var spriteFrame=spriteSource.getSpriteFrame(progressbar.barSprite.spriteFrame);
+        str+='progressbar_spriteFrame:'+spriteFrame+'%o__%';
         str+='progressbar_mode:'+progressbar.mode+'%o__%';
-        str+='progressbar_totalLength:'+progressbar.totalLength+'%o__%';
         str+='progressbar_progress:'+progressbar.progress+'%o__%';
         str+='progressbar_reverse:'+progressbar.reverse+'%o__%';
     }
     return str;
 }
+module.exports=getProgressBar;
