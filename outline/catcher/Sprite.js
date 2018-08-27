fbdd32b930bc1ba2e7e63c3eea992c361efc219f
limall <2497902705@qq.com>
2018-08-27 09:52:00 +0800

    当creator的Sprite类型的SpriteFrame为空时，不导出为sprite类型

--- outline/catcher/Sprite.js
+++ outline/catcher/Sprite.js
@@ -3,19 +3,21 @@ var getValueStr=require('./ValueStr');
 
 module.exports=function(sprite,typeInfo){
     var spriteFrame=sprite.spriteFrame;
+    
+    if(spriteFrame){
+        typeInfo.add('isSprite',getValueStr(true));
+        typeInfo.add('sprite_spriteFrame',getValueStr(spriteFrame));
 
-    typeInfo.add('isSprite',getValueStr(true));
-    typeInfo.add('sprite_spriteFrame',getValueStr(spriteFrame));
-
-    if(sprite.type===cc.Sprite.Type.SLICED){
-        var insetTop=spriteFrame.insetTop;
-        var insetBottom=spriteFrame.insetBottom;
-        var insetLeft=spriteFrame.insetLeft;
-        var insetRight=spriteFrame.insetRight;
-        typeInfo.add('sprite_isSliced',getValueStr(true));
-        typeInfo.add('sprite_insetTop',getValueStr(insetTop));
-        typeInfo.add('sprite_insetBottom',getValueStr(insetBottom));
-        typeInfo.add('sprite_insetLeft',getValueStr(insetLeft));
-        typeInfo.add('sprite_insetRight',getValueStr(insetRight));
+        if(sprite.type===cc.Sprite.Type.SLICED){
+            var insetTop=spriteFrame.insetTop;
+            var insetBottom=spriteFrame.insetBottom;
+            var insetLeft=spriteFrame.insetLeft;
+            var insetRight=spriteFrame.insetRight;
+            typeInfo.add('sprite_isSliced',getValueStr(true));
+            typeInfo.add('sprite_insetTop',getValueStr(insetTop));
+            typeInfo.add('sprite_insetBottom',getValueStr(insetBottom));
+            typeInfo.add('sprite_insetLeft',getValueStr(insetLeft));
+            typeInfo.add('sprite_insetRight',getValueStr(insetRight));
+        }
     }
 }
